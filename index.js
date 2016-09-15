691cd6fcb2b985fd502306712ed8f06b5f5f8066
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-09-15 21:54:44 +0200

    Use hosted-git-info to simplify code.

--- index.js
+++ index.js
@@ -33,58 +33,21 @@ function getProjectDependencies(packageJSON) {
 
 function getProjectUserRepo(packageJSON) {
     if (!packageJSON.repository) {
+        // If the package does not have any repository field, return null
         return null;
     }
 
-    if (typeof packageJSON.repository === 'string') {
-        let repoStr = packageJSON.repository;
-
-        if (repoStr.indexOf(':') !== -1) {
-            // This means that the location is not on Github.
-            // As Github does not allow ':' in the names and the other services
-            // use it as a delimiter.
-            return null;
-        }
-
-        let userRepo = repoStr.split('/');
+    const info = hostedGitInfo.fromUrl(typeof packageJSON.repository === 'string' ?
+                                       packageJSON.repository :
+                                       packageJSON.repository.url);
+    if (info && info.type === 'github') {
         return {
-            user: userRepo[0],
-            repo: userRepo[1]
+            user: info.user,
+            repo: info.project
         };
     }
 
-    if (!packageJSON.repository.type || !packageJSON.repository.url) {
-        // Most likely malformed package.json
-        return null;
-    }
-
-    let repoType = packageJSON.repository.type.toLowerCase();
-    let repoUrl = packageJSON.repository.url.toLowerCase();
-
-    if (repoType !== 'git') {
-        // We are looking only for Github
-        return null;
-    }
-
-    let idxOfGithubCom = repoUrl.indexOf('github.com');
-
-    if (idxOfGithubCom === -1) {
-        // If the URL does not contain github.com, return null
-        return null;
-    }
-
-    let userRepo = repoUrl
-         // Gobble up the github.com + 1 delimiter
-        .substr(idxOfGithubCom + 'github.com'.length + 1)
-         // Replace the .git at the end with ''
-        .replace(/\.git$/, '')
-         // Split at the '/'
-        .split('/');
-
-    return {
-        user: userRepo[0],
-        repo: userRepo[1]
-    };
+    return null;
 }
 
 module.exports = {
