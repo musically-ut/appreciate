9ac6b437437558f42ed62d74abc8810b43c14a3a
Utkarsh Upadhyay <musically.ut@gmail.com>
2016-09-19 00:15:16 +0200

    Use the multispinner package to improve CLI output.

--- cli.js
+++ cli.js
@@ -2,16 +2,56 @@
 'use strict';
 
 const fs = require('mz/fs');
+const Multispinner = require('multispinner');
 const expandHomeDir = require('expand-home-dir');
 const chalk = require('chalk');
 
 const api = require('./');
 
+function getGithubName(moduleInfo) {
+    return moduleInfo.githubInfo.user + '/' + moduleInfo.githubInfo.repo;
+}
+
+function makePadding(spaces) {
+    if (spaces === 0) {
+        return '';
+    } else if (spaces % 2 === 0) {
+        const ch = makePadding(spaces / 2);
+        return ch + ch;
+    }
+
+    return makePadding(spaces - 1) + ' ';
+}
+
+function makeTask(token, moduleInfo, multiSpinners, maxPad) {
+    const githubName = getGithubName(moduleInfo);
+    const padding = makePadding(maxPad - githubName.length + 1);
+
+    return api.isAppreciated(token, moduleInfo).then(
+        x => {
+            if (x.error) {
+                multiSpinners.spinners[githubName].text += padding + chalk.red(x.error);
+                multiSpinners.error(githubName);
+                return;
+            }
+
+            multiSpinners.spinners[githubName].text += padding + (x.starred ? chalk.blue('★ Starred.') : chalk.yellow('✩ Not starred!'));
+            multiSpinners.success(githubName);
+            return Promise.resolve();
+        },
+        err => {
+            multiSpinners.spinners[githubName].text += padding + err;
+            multiSpinners.error(githubName);
+            return Promise.reject(err);
+        }
+    );
+}
+
 // No command line arguments so far.
 api.readAuthToken()
 .then(
     token => {
-        console.info('Reading package.json ...');
+        console.info('Project dependencies: ');
         fs.readFile('./package.json')
         .then(
             data => {
@@ -26,21 +66,22 @@ api.readAuthToken()
                 return Promise.reject(null);
             }
         ).then(moduleInfos => {
-            return api.areAppreciated(token, moduleInfos);
-        }).then(starredInfos => {
-            starredInfos.forEach(x => {
-                if (x.error) {
-                    console.error(chalk.red(x.moduleName), '\t:', x.error);
-                    return;
-                }
-
-                let githubName = x.githubInfo.user + '/' + x.githubInfo.repo;
-                if (x.starred) {
-                    console.log(chalk.green(x.moduleName), '\t:', githubName, 'is starred!');
-                } else {
-                    console.log(chalk.yellow(x.moduleName), '\t:', githubName, 'is not starred!');
-                }
+            const spinnerNames = moduleInfos.map(x => {
+                return getGithubName(x);
             });
+
+            const maxPadding = Math.max.apply(
+                null,
+                spinnerNames.map(x => {
+                    return x.length;
+                })
+            );
+
+            const spinners = new Multispinner(spinnerNames, {clear: false, autoStart: true});
+
+            return Promise.all(moduleInfos.map(x => {
+                return makeTask(token, x, spinners, maxPadding);
+            }));
         }).catch(err => {
             console.error(chalk.red('Error: '), err);
         });
